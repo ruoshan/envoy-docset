be43c6e7207fb0d0f7228ca9efe1e2b2c6b1d36c
Ruoshan Huang <ruoshan.huang@gmail.com>
2019-03-20 17:13:58 +0800

    Add v1.9.0 doc

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+rm -rf Envoy-$1.docset
 cp -r Envoy.docset-tmpl Envoy-$1.docset
 sed -i "" -e "s/VERSION/$1/g" Envoy-$1.docset/Contents/Info.plist
 cp -r Documents/$1 Envoy-$1.docset/Contents/Resources/Documents/
